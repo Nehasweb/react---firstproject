fa1247032db0361d2688ebb36b8f6aae1955ad35
Neha Gholap <nehagholap98@gmail.com>
2024-02-29 21:42:25 +0530

    First ReactProject

---
+++ src/Components/Infosystems.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Infosystems extends Component
+{
+    render()
+    {
+        return <h1>Inside Infosystems Component (Class Component)</h1>
+    }
+}
+
+export default Infosystems
---
+++ src/Components/Marvellous.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Marvellous()
+{
+    return <h1>Inside Marvellous Component (functional Component) </h1>
+}
+
+
+export default Marvellous 
